61d20f701690c108964e875fab24a8e71274119c
Brian Pfeil <brian.pfeil@gmail.com>
2021-04-26 09:47:46 -0400

    init

--- lib/aws-cdk-step-functions-playground-stack.ts
+++ lib/aws-cdk-step-functions-playground-stack.ts
@@ -1,9 +1,32 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import { NodejsFunction } from "@aws-cdk/aws-lambda-nodejs";
+import * as sfn from "@aws-cdk/aws-stepfunctions";
+import * as lambda from "@aws-cdk/aws-lambda";
+import * as tasks from "@aws-cdk/aws-stepfunctions-tasks";
 
 export class AwsCdkStepFunctionsPlaygroundStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const fn = new lambda.Function(this, "myFn", {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      handler: "index.handler",
+      code: lambda.Code.fromInline(`
+      exports.handler = async (event) => {
+        console.log(event);
+        return { statusCode: 201, body: 'Hello world!' };
+      };
+      `),
+    });
+
+    const lambdaTask = new tasks.LambdaInvoke(this, "Invoke Handler", {
+      lambdaFunction: fn,
+    });
+
+    const definition = lambdaTask;
+
+    new sfn.StateMachine(this, "StateMachine", {
+      definition,
+    });
   }
 }
